d67392d149840a62edea326a67453aa12a5d9db7
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2023-03-15 00:38:05 +0530

    update

--- src/app.js
+++ src/app.js
@@ -4,22 +4,40 @@ const app = express()
 
 
 // Your code goes here
-const subscribersData = require('./data')
+const Subscriber = require('./models/subscribers');
 
 // GET http://localhost:3000/subscribers
 app.get('/subscribers', (req, res) => {
-    res.send(subscribersData)
+    Subscriber.find()
+        .then((data) => {
+            res.send(data)
+        })
+        .catch((err) => {
+            res.send(err)
+        })
 })
-
 // GET http://localhost:3000/subscribers/names
 app.get('/subscribers/names', (req, res) => {
-    res.send(subscribersData)
+    Subscriber.find()
+        .then((data) => {
+            res.send(data.map(el => ({ name: el.name, subscribedChannel: el.subscribedChannel })))
+
+        })
+
 })
 
 // GET http://localhost:3000/subscribers/:id
 app.get('/subscribers/:id', (req, res) => {
-    const subscriber = subscribersData.find((sub) => sub.id === +req.params.id)
-    res.send(subscriber)
+    console.log(req.params);
+    const { id } = req.params
+    Subscriber.find({ _id: id })
+        .then(data => {
+            console.log(data)
+            res.send(data)
+        })
+        .catch((error) => {
+            res.send({message: error.message})
+        })
 })
 
 
