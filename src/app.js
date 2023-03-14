12da2aa0e8626ecbe7dbe99b89fd974becf57eaf
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2023-03-14 20:58:08 +0530

    update

--- src/app.js
+++ src/app.js
@@ -4,7 +4,23 @@ const app = express()
 
 
 // Your code goes here
+const subscribersData = require('./data')
 
+// GET http://localhost:3000/subscribers
+app.get('/subscribers', (req, res) => {
+    res.send(subscribersData)
+})
+
+// GET http://localhost:3000/subscribers/names
+app.get('/subscribers/names', (req, res) => {
+    res.send(subscribersData)
+})
+
+// GET http://localhost:3000/subscribers/:id
+app.get('/subscribers/:id', (req, res) => {
+    const subscriber = subscribersData.find((sub) => sub.id === +req.params.id)
+    res.send(subscriber)
+})
 
 
 
